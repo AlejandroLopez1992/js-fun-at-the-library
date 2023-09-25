35c99d7da5f478dfbd1c4eeed442bcb99e37ef6e
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-09-25 14:52:38 -0600

    Feat: Complete book.js exercises

--- src/book.js
+++ src/book.js
@@ -1,12 +1,47 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(text, reviews) {
+  if (!reviews.includes(text)) {
+    reviews.push(text) 
+  } 
+}
+
+function calculatePageCount (bookTitle) {
+  var stringLength = bookTitle.length;
+  var multipliedLength = stringLength * 20;
+  return multipliedLength
+}
+
+function writeBook (title, mainCharacter, genre ) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(title)
+  }
+}
+
+function editBook(book) {
+  var pageCount = book.pageCount
+  var editedpageCount = pageCount * .75
+  book['pageCount'] = editedpageCount
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
