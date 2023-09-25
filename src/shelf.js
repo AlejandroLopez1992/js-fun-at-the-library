7457c1c7caa3162601ff40bcb8581d6e398be3dd
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-09-25 15:59:20 -0600

    Feat: Complete shelf.js exercises

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook (book, shelf) {
+  if (shelf.length <= 2) {
+    shelf.unshift(book);
+  }
+}
+
+
+function unshelfBook(bookName, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookName) {
+      shelf.splice(i, 1);
+      break;
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = shelf.map(function(book) {
+    return book.title;
+  });
+  return titles.join(", ");
+}
+
+function searchShelf (shelf, bookName) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookName) {
+      return true;
+    } 
+  }
+  return false;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
